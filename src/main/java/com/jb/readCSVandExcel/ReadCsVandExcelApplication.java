ea8f985ccdd0ec869a16196068e5381147ec0cd1
Jean_Burda <jean@sherpaerp.com>
2021-05-10 21:13:08 -0300

    done

---
+++ src/main/java/com/jb/readCSVandExcel/ReadCsVandExcelApplication.java
@@ -0,0 +1,15 @@
+package com.jb.readCSVandExcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadCsVandExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadCsVandExcelApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/jb/readCSVandExcel/ReadCsVandExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.readCSVandExcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadCsVandExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
